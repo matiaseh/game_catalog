ce40e87fb9116e6e4ddf75e8ad1692fa686a92ea
Matias Hynnä <hynna.matias@gmail.com>
2024-11-02 13:47:29 +0200

    Add navigation between iews to navbar

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,9 +1,16 @@
 import { useAuth } from '../../context/AuthContext';
 import logo from '../../assets/finnplayLogo.svg';
 import styles from './NavBar.module.scss';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const NavBar = () => {
   const { logout } = useAuth();
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
 
   const handleLogout = () => {
     logout();
@@ -12,6 +19,21 @@ const NavBar = () => {
   return (
     <nav className={styles.navBar}>
       <img src={logo} className={styles.logo} />
+      <div className={styles.menuIcon} onClick={toggleDropdown}>
+        <i className="fa-solid fa-bars" />
+      </div>
+      <ul className={`${styles.navList} ${isDropdownOpen ? styles.show : ''}`}>
+        <li>
+          <Link to="/player" onClick={() => setIsDropdownOpen(false)}>
+            Player
+          </Link>
+        </li>
+        <li>
+          <Link to="/player/groups" onClick={() => setIsDropdownOpen(false)}>
+            Groups
+          </Link>
+        </li>
+      </ul>
       <button onClick={() => handleLogout()} className={styles.logoutButton}>
         <i className="fa-regular fa-user" />
         Logout
